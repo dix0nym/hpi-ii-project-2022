8bd8b831b8814b4774deb3601b88c001fb3acdc6
Lars Prepens <lars.prepens@student.hpi.uni-potsdam.de>
2022-05-26 11:54:24 -0400

    HRB parser

---
+++ get_sample.sh
@@ -0,0 +1,17 @@
+poetry run python rb_crawler/main.py --id 1 --state by --stop_id 200
+poetry run python rb_crawler/main.py --id 1 --state bw --stop_id 200
+poetry run python rb_crawler/main.py --id 1 --state be --stop_id 200
+poetry run python rb_crawler/main.py --id 1 --state br --stop_id 200
+poetry run python rb_crawler/main.py --id 1 --state hb --stop_id 200
+poetry run python rb_crawler/main.py --id 1 --state hh --stop_id 200
+poetry run python rb_crawler/main.py --id 1 --state he --stop_id 200
+poetry run python rb_crawler/main.py --id 1 --state mv --stop_id 200
+poetry run python rb_crawler/main.py --id 1 --state ni --stop_id 200
+poetry run python rb_crawler/main.py --id 1 --state nw --stop_id 200
+poetry run python rb_crawler/main.py --id 1 --state rp --stop_id 200
+poetry run python rb_crawler/main.py --id 1 --state sl --stop_id 200
+poetry run python rb_crawler/main.py --id 1 --state sn --stop_id 200
+poetry run python rb_crawler/main.py --id 1 --state st --stop_id 200
+poetry run python rb_crawler/main.py --id 7831 --state sh --stop_id 8030
+poetry run python rb_crawler/main.py --id 1 --state th --stop_id 200
+
