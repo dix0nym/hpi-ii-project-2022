cb0116f5c15553ffd637b0b3d4f7006be23c5c64
dix0nym <dixonym@mailbox.org>
2022-06-27 16:30:43 +0200

    Remove include directory

--- generate-proto.sh
+++ generate-proto.sh
@@ -2,5 +2,5 @@
 
 protoc --proto_path=proto --python_out=build/gen proto/bakdata/corporate/v1/corporate.proto
 protoc --proto_path=proto --python_out=build/gen proto/parsed_hrb/v1/*.proto
-protoc --proto_path=proto -I include/ --python_out=build/gen proto/gleif/v1/*.proto
+protoc --proto_path=proto --python_out=build/gen proto/gleif/v1/*.proto
 find build/gen -type f -name '*.py' -exec sed -i 's/from gleif.v1/from build.gen.gleif.v1/g' {} +
