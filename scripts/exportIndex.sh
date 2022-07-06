6ab397d78d61b48381f9838050aa881b89403075
dix0nym <dixonym@mailbox.org>
2022-07-06 13:21:04 +0200

    updated exportIndex.sh

--- scripts/exportIndex.sh
+++ scripts/exportIndex.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 5 ]; then
     echo "Provide args: $0 [index] [filename]"
     exit 1
 fi
 
 index=$1
 filename=$2
+username=$3
+password=$4
+path=$5
 
 echo "dumping $index"
 sudo elasticdump --input="http://localhost:9200/$index" --output=$ | gzip > "$filename"
 
 echo "uploading "$filename" to owncloud"
-curl -X PUT -u $username:$password --progress-bar -T "$filename" "https://owncloud.hpi.de/remote.php/dav/files/$username/InfoIntegration/datasets/$filename" | cat
+curl -X PUT -u "$username:$password" --progress-bar -T "$filename" "https://owncloud.hpi.de/remote.php/dav/files/$username/$path/$filename" | cat
