a69fcda7a421dc30782491a072eba8c01ec5a239
dix0nym <dixonym@mailbox.org>
2022-07-09 20:37:33 +0000

    updated scripts - arg 'all'

--- scripts/exportIndex.sh
+++ scripts/exportIndex.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 
-if [ "$#" -ne 5 ]; then
-    echo "Provide args: $0 [index] [filename]"
+if [ "$#" -ne 4 ]; then
+    echo "Provide args: $0 [index] [username] [password] [path]"
     exit 1
 fi
 
 index=$1
-filename=$2
-username=$3
-password=$4
-path=$5
+username=$2
+password=$3
+path=$4
 
-echo "dumping $index"
-sudo elasticdump --input="http://localhost:9200/$index" --output=$ | gzip > "$filename"
+exportIndex() {
+	index=$1
+	username=$2
+	password=$3
+	path=$4
+	timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+	filename="$1_$timestamp.json.gz"
 
-echo "uploading "$filename" to owncloud"
-curl -X PUT -u "$username:$password" --progress-bar -T "$filename" "https://owncloud.hpi.de/remote.php/dav/files/$username/$path/$filename" | cat
+	echo "dumping $index"
+	sudo elasticdump --input="http://localhost:9200/$index" --output=$ | gzip > "$filename"
+	echo "uploading $filename"
+	curl -X PUT -u "$username:$password" --progress-bar -T "$filename" "https://owncloud.hpi.de/remote.php/dav/files/$username/$path/$filename" | cat
+}
+
+
+if [[ $index == "all" ]];then
+	cmd=$(curl --silent 'http://127.0.0.1:9200/_cat/indices' | cut -d\  -f3)
+	readarray -t indices <<< "$cmd"
+        for i in "${indices[@]}"; do
+		exportIndex $i $username $password $path
+	done
+else
+	exportIndex $index $username $password $path
+fi
--- scripts/makeTopicForever.sh
+++ scripts/makeTopicForever.sh
@@ -5,6 +5,22 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-topic=$1
-kafka-configs.sh --bootstrap-server localhost:29092 --entity-type topics --entity-name "$topic" --alter --add-config retention.ms=-1
-kafka-configs.sh --bootstrap-server localhost:29092 --entity-type topics --entity-name "$topic" --alter --add-config retention.bytes=-1
+setTopicRetention() {
+	topic=$1
+	value=$2
+	kafka-configs.sh --bootstrap-server localhost:29092 --entity-type topics --entity-name "$topic" --alter --add-config retention.ms="$value"
+	kafka-configs.sh --bootstrap-server localhost:29092 --entity-type topics --entity-name "$topic" --alter --add-config retention.bytes="$value"
+}
+
+if [ "$1" == "all" ]; then
+	cmd=$(kafka-topics.sh  --bootstrap-server=localhost:29092 --list)
+	readarray -t topics <<< "$cmd"
+	for i in "${topics[@]}"; do
+		if [[ "$i" =~ ^[^_].* ]]; then
+			echo "setting retention to -1 for ${i}"
+			setTopicRetention "$i" "-1"
+		fi
+	done
+else
+	setTopicRetention $1 "-1"
+fi
